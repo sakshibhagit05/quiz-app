9985f88c5d52e715281500ff9edb7559f0bfcd65
Sakshi <sakshibhagit@gmail.com>
2026-07-13 15:25:37 +0530

    Updated Quiz

--- src/pages/CreateQuiz.jsx
+++ src/pages/CreateQuiz.jsx
@@ -230,13 +230,14 @@ function CreateQuiz() {
               </h2>
 
               {questions.length > 1 && (
-                <button
-                  onClick={() => removeQuestion(index)}
-                  className="bg-red-500 text-white px-3 py-1 rounded"
-                >
-                  Delete
-                </button>
-              )}
+  <button
+    type="button"
+    onClick={() => removeQuestion(index)}
+    className="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded-lg"
+  >
+    Delete Question
+  </button>
+)}
 
             </div>
 
