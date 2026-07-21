1ba7a81043c97ec33a5d417e615eb57809fc8ed5
Sakshi <sakshibhagit@gmail.com>
2026-07-21 21:08:10 +0530

    Updated quiz app

--- src/pages/StudentLogin.jsx
+++ src/pages/StudentLogin.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const subjects = [
-  "Mathematics",
+  "Math",
   "English",
   "Marathi",
   "शिष्यवृत्ती मराठी",
